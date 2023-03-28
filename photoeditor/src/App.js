3d586076e07270b67d02c627387373693c745f0c
Marx-wrld <iongonga@kabarak.ac.ke>
2023-03-28 11:51:38 +0300

    setting the sidebar functionality

--- photoeditor/src/App.js
+++ photoeditor/src/App.js
@@ -1,15 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Slider from './Slider';
 import SidebarItem from './SidebarItem'
 
+const DEFAULT_OPTIONS = [ //we are going to take this DefaultOptions and turn it to an options object we can use in react and actually modify and by that we will need a state
+  {
+    name: 'Brightness',
+    property: 'brightness',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Contrast',
+    property: 'contrast ',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Saturation',
+    property: 'saturation',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Grayscale',
+    property: 'grayscale',
+    value: 0, //we don't want a default 100 value for grayscale 
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
+  {
+    name: 'Sepia',
+    property: 'sepia',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Hue Rotate',
+    property: 'hue-rotate',
+    value: 100,
+    range: {
+      min: 0,
+      max: 360 //360 because we have 360 deg of hue
+    },
+    unit: 'deg'
+  },
+  {
+    name: 'Blur',
+    property: 'blur',
+    value: 100,
+    range: {
+      min: 0,
+      max: 50
+    },
+    unit: 'px'
+  }
+]
+
 function App() {
+
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
+  const [options, setOptions] = useState(DEFAULT_OPTIONS)
+  const selectedOption = options[selectedOptionIndex]
+
   return (
     <div className="container">
       <div className="main-image" />
       <div className="sidebar">
-        <SidebarItem />
-        <SidebarItem />
+
+        {options.map((option, index) => {
+          return (
+          <SidebarItem 
+            key={index}
+            name={option.name}
+            active={index === selectedOptionIndex} //our active property is going to be true if our index = selectedOptionIndex
+            handleClick={() => {
+              setSelectedOptionIndex(index) //now whenenver we click on an item, its going to be set active as our selected item
+            }}
+          />
+          )
+          })}
+
         </div>
         <Slider />
     </div>
--- photoeditor/src/SidebarItem.js
+++ photoeditor/src/SidebarItem.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-export default function SidebarItem() {
+export default function SidebarItem({name, active, handleClick}) {
   return (
-    <button className="sidebar-item">Sidebar Item</button> //active helps by highlighting an item when hovering
+    <button className={`sidebar-item ${active ? 'active' : ''}`}
+    onClick={handleClick}
+    >{name}</button> //active helps by highlighting an item when hovering. So, here its going to give us an active class if active is true else it will return an empty string
   )
 }
 
