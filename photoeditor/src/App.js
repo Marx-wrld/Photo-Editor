75d7372c4a6de93a6a7f6e389ed1a4d1c4e07b09
Marx-wrld <iongonga@kabarak.ac.ke>
2023-03-28 13:19:48 +0300

    Finishing up and debug

--- photoeditor/src/App.js
+++ photoeditor/src/App.js
@@ -26,7 +26,7 @@ const DEFAULT_OPTIONS = [ //we are going to take this DefaultOptions and turn it
   },
   {
     name: 'Saturation',
-    property: 'saturation',
+    property: 'saturate',
     value: 100,
     range: {
       min: 0,
@@ -82,9 +82,29 @@ function App() {
   const [options, setOptions] = useState(DEFAULT_OPTIONS)
   const selectedOption = options[selectedOptionIndex]
 
+  function handleSliderChange({ target }) // event where we get our value from, and all we really need from the event is our target
+  {
+    setOptions(PrevOptions => { //calling our setOptions and we need make sure we use our prevOptionsas a frame to this function 
+      return PrevOptions.map((option, index) => { //we then return our prevOptions and loop through our Options until we find the one that we want to change then we change and update it 
+        if (index !== selectedOptionIndex) return option //if our index is not equal to our selectedIndex, we know that it is not the option we have  selected and threfore return it as is
+        return {
+         ...option, value: target.value //otherwise if its the one we have selected, we want to return a new object for our option, which is going to contain everything from our old option and what we want to do is set the value from our slider(target)
+        }
+      })
+    })
+  } 
+  
+  function getImageStyle(){ //This function takes all the values and adds them to our image to apply the filters
+    const filters = options.map(option => {//looping through all our options to get them converted to a css property 
+      return `${option.property}(${option.value}${option.unit})` //That's now everything for creating the string of our different filters
+    })
+    //converting from an array to a style object
+    return { filter: filters.join(' ')} //putting a space before each of our different array strings
+  }
+
   return (
     <div className="container">
-      <div className="main-image" />
+      <div className="main-image" style={getImageStyle()}/>
       <div className="sidebar">
 
         {options.map((option, index) => {
@@ -101,7 +121,12 @@ function App() {
           })}
 
         </div>
-        <Slider />
+        <Slider //passing all the values here
+          min={selectedOption.range.min}
+          max={selectedOption.range.max}
+          value={selectedOption.value}
+          handleChange={handleSliderChange}
+        />
     </div>
   );
 }
