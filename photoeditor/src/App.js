484e5f96a62ff65695e9b822215bd7dec11a5a1b
Marx-wrld <iongonga@kabarak.ac.ke>
2023-03-29 09:23:45 +0300

    Bg-image url debug

--- photoeditor/src/App.js
+++ photoeditor/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Slider from './Slider';
-import SidebarItem from './SidebarItem'
+import SidebarItem from './SidebarItem';
 
 const DEFAULT_OPTIONS = [ //we are going to take this DefaultOptions and turn it to an options object we can use in react and actually modify and by that we will need a state
   {
@@ -16,7 +16,7 @@ const DEFAULT_OPTIONS = [ //we are going to take this DefaultOptions and turn it
   },
   {
     name: 'Contrast',
-    property: 'contrast ',
+    property: 'contrast',
     value: 100,
     range: {
       min: 0,
@@ -67,7 +67,7 @@ const DEFAULT_OPTIONS = [ //we are going to take this DefaultOptions and turn it
   {
     name: 'Blur',
     property: 'blur',
-    value: 100,
+    value: 0,
     range: {
       min: 0,
       max: 50
@@ -84,9 +84,9 @@ function App() {
 
   function handleSliderChange({ target }) // event where we get our value from, and all we really need from the event is our target
   {
-    setOptions(PrevOptions => { //calling our setOptions and we need make sure we use our prevOptionsas a frame to this function 
-      return PrevOptions.map((option, index) => { //we then return our prevOptions and loop through our Options until we find the one that we want to change then we change and update it 
-        if (index !== selectedOptionIndex) return option //if our index is not equal to our selectedIndex, we know that it is not the option we have  selected and threfore return it as is
+    setOptions(prevOptions => { //calling our setOptions and we need make sure we use our prevOptionsas a frame to this function 
+      return prevOptions.map((option, index) => { //we then return our prevOptions and loop through our Options until we find the one that we want to change then we change and update it 
+        if (index !== selectedOptionIndex) return option //if our index is not equal to our selectedIndex, we know that it is not the option we have  selected and threfore return it as is.
         return {
          ...option, value: target.value //otherwise if its the one we have selected, we want to return a new object for our option, which is going to contain everything from our old option and what we want to do is set the value from our slider(target)
         }
@@ -113,14 +113,15 @@ function App() {
             key={index}
             name={option.name}
             active={index === selectedOptionIndex} //our active property is going to be true if our index = selectedOptionIndex
-            handleClick={() => {
-              setSelectedOptionIndex(index) //now whenenver we click on an item, its going to be set active as our selected item
-            }}
+            handleClick={() => 
+              setSelectedOptionIndex(index) //now whenenver we click on an item, its going to be set active as our selected item.
+            }
           />
           )
           })}
 
         </div>
+        
         <Slider //passing all the values here
           min={selectedOption.range.min}
           max={selectedOption.range.max}
--- photoeditor/src/SidebarItem.js
+++ photoeditor/src/SidebarItem.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function SidebarItem({name, active, handleClick}) {
   return (
     <button className={`sidebar-item ${active ? 'active' : ''}`}
     onClick={handleClick}
-    >{name}</button> //active helps by highlighting an item when hovering. So, here its going to give us an active class if active is true else it will return an empty string
+    >
+      {name}
+      </button> //active helps by highlighting an item when hovering. So, here its going to give us an active class if active is true else it will return an empty string
   )
 }
-
